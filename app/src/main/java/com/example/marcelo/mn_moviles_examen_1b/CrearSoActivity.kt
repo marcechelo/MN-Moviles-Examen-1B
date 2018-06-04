fecf43389b806b1b8538e050ed267d259a27b6b1
Marcelo Nieto <marcechelo25@hotmail.com>
2018-06-03 21:41:25 -0500

    crearSo

--- app/src/main/java/com/example/marcelo/mn_moviles_examen_1b/CrearSoActivity.kt
+++ app/src/main/java/com/example/marcelo/mn_moviles_examen_1b/CrearSoActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_crear_so.*
 import java.util.*
 
@@ -15,23 +16,25 @@ class CrearSoActivity : AppCompatActivity() {
 
         //val dbHandler = DbHandlerAplicacion(this)
 
-        MainActivity.BaseDeDatos.crearSo("algo1",1, "09/04/2010",1.1,true)
-        MainActivity.BaseDeDatos.leer()
+        //MainActivity.BaseDeDatos.crearSo("algo1",1, "09/04/2010",1.1,true)
+        //MainActivity.BaseDeDatos.leer()
 
         boton_guardar.setOnClickListener{view: View ->
-            val nombre = edit_nombre.text.toString()
-            val version = edit_version_api.text.toString()
 
-            /*val fecha = edit_fecha.text.toString()
-            val api = edit_version_api.text.toString().toDouble()
-            val instaldo:Boolean
-            if (cb_instalado.isChecked()){instaldo = true} else instaldo = false*/
-            //MainActivity.BaseDeDatos.crearSo(nombre,version1,fecha,api,instaldo)
-            //MainActivity.BaseDeDatos.crearSo("algo",1, "09/04/2010",1.1,true)
-            //MainActivity.BaseDeDatos.leer()
-            MainActivity.BaseDeDatos.crearSo("algo1",1, "09/04/2010",1.1,true)
-
-            irAActividadListarOs()
+            if(edit_nombre.text.toString().isEmpty() ||
+                    edit_version_api.text.toString().isEmpty() ||
+                    edit_fecha.text.toString().isEmpty() ||
+                    edit_version_api.text.toString().isEmpty()){
+                val toast = Toast.makeText(this,"Todos los campos deben estra lleno",Toast.LENGTH_SHORT)
+                toast.show()
+            }else{
+                val nombre = edit_nombre.text.toString()
+                val version = edit_version_api.text.toString().toDouble()
+                val fecha = edit_fecha.text.toString()
+                val api = edit_version_api.text.toString().toDouble()
+                val instaldo:Boolean = cb_instalado.isChecked()
+                irAActividadListarOs()
+            }
         }
     }
 
